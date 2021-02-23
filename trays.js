c794d368e2f3c55bc02b293262e1f03bef5cc54b
Ryan Prescott <rppgoat@gmail.com>
2021-02-22 19:24:32 -0500

    Trays act like trays

--- trays.js
+++ trays.js
@@ -22,32 +22,29 @@ class TraysParentException extends Error {
 
 class Trays {
     constructor(element) {
-        if (element instanceof Element) {
-            if (Trays._empty(element.id)) {
-                throw new TraysElementException('Element must have id.');
-            } else {
-                this.domElement = element;
-                element = undefined;
-                this.id = this.domElement.id;
-                this.enumerateChildren(this.domElement);
-            }
-            
-        } else {
+        if (!element instanceof Element) {
             throw new TraysDOMException('The object is not an instance of Element.');
+        } else if (!element?.classList.contains('trays')) {
+            throw new TraysDOMException('Element must have "trays" class.');
+        } else if (Trays._empty(element.id)) {
+            throw new TraysDOMException('Element must have id.');
+        } else {
+            this.domElement = element;
+            this.id = element.id;
+            this.isFullscreen = false;
+            this.enumerateChildren();
         }
     }
 
-    static classMap = {
-        'open': (child)=>{child.parent.active[child.id]=child}
-    };
-
     static hashMap = {
-        '+': (tray)=>{tray.open&&tray.open()},
-        '-': (tray)=>{tray.close&&tray.close()},
-        '~': (tray)=>{tray.toggle&&tray.toggle()}
+        '+': (tray)=>{tray?.open()},
+        '-': (tray)=>{tray?.close()},
+        '~': (tray)=>{tray?.toggle()}
     };
 
-    isFullscreen = false;
+    static classMap = {
+        'open': (tray)=>{tray?.open()}
+    };
 
     static load() {
         var traysCol = {};
@@ -55,7 +52,7 @@ class Trays {
         for (const traysElement of traysElements) {
             var traysObject = new Trays(traysElement);
             if (traysCol[traysObject.id]) {
-                throw new TraysDuplicateException('Trays "' + traysObject.id + '" already exists in the tray collection.')
+                throw new TraysDuplicateException('Trays "' + traysObject.id + '" already exists in the tray collection.');
             } else {
                 traysCol[traysObject.id] = traysObject;
             }
@@ -66,8 +63,8 @@ class Trays {
 
     static hash(g) {
         var h = window.location.hash;
-        if ((h = h.slice(1)).slice(0, 2) == '::') {
-            h = h.slice(2);
+        if (h.slice(0, 3) === '#::') {
+            h = h.slice(3);
             var i = Object.keys(g), j = h.split('$'), k = g[j[0]] || g[i[j[0]]] || g[i[0]] || null;
             if (k !== null) {
                 var l = j[j.length === 1 ? 0 : 1].split('|');
@@ -79,7 +76,6 @@ class Trays {
                 }
             }
         }
-        window.location.hash = '';
     }
 
     static _empty(obj) {
@@ -88,20 +84,14 @@ class Trays {
 
     enumerateChildren() {
         this.children = {};
-        this.active = {};
+        this.open = {};
+        var childArray = Array.from(this.children);
         for (const child of this.domElement.children) {
-            if (child.classList && child.classList.contains('tray')) {
-                if (Trays._empty(child.id)) {
-                    throw new TraysDOMException('Element must have id.');
-                } else if (this.children[child.id]) {
-                    throw new TraysDuplicateException('Tray "' + child.id + '" already exists in Trays object "' + this.id + "'.");
-                } else {
-                    var tray = new Tray(child, this);
-                    this.children[child.id] = tray;
-                    child.classList.forEach((x)=>{Trays.classMap[x]&&Trays.classMap[x](tray)}, this)
-                }
+            var tray = new Tray(child, this);
+            if (childArray.includes(tray)) {
+                throw new TraysDuplicateException('Tray "' + tray.id + '" already exists in Trays object "' + this.id + "'.");
             } else {
-                child.remove();
+                this.children[tray.id] = tray;
             }
         }
     }
@@ -109,6 +99,18 @@ class Trays {
     clear() {
         this.domElement.innerHTML = '';
         this.enumerateChildren();
+    }
+
+    next() {
+        // Get the next inactive child through convoluted means
+        var x = this.children[Object.keys(this.children).filter(x => !Object.keys(this.open).includes(x))[0]];
+        return x?.open() ? true : false;
+    }
+
+    previous() {
+        // Get the currently active child through less convoluted means
+        var x = this.open[Object.keys(this.open).slice(-1)];
+        return x?.close() ? true : false;
     }
 
     fullscreen() {
@@ -137,27 +139,37 @@ class Trays {
 class Tray {
     constructor(element, parent) {
         if (!parent instanceof Trays) {
-            throw new TraysParentException('The parent object must be an instance of Trays.');
-        } else if (element instanceof Element) {
+            throw new TraysParentException('The parent must be a Trays object.');
+        } else if (!element instanceof Element) {
+            throw new TraysDOMException('The object is not an instance of Element.');
+        } else if (!element?.classList.contains('tray')) {
+            throw new TraysDOMException('Element must have "tray" class.');
+        } else if (Trays._empty(element.id)) {
+            throw new TraysDOMException('Element must have id.');
+        } else {
             this.domElement = element;
             this.id = element.id;
             this.parent = parent;
-        } else {
-            throw new TraysElementException('The object is not an instance of Element.');
+            for(const className of this.domElement.classList) {
+                Trays.classMap[className]&&Trays.classMap[className](this);
+            }
         }
     }
+    get isOpen() {
+        return this.parent.open[this.id] ? true: false;
+    }
     open() {
-        !this.parent.active[this.id] && (this.parent.active[this.id] = this, this.domElement.classList.add('open'));
+        return !this.isOpen && (this.parent.open[this.id] = this, this.domElement.classList.add('open')) || true;
     }
     close() {
-        this.parent.active[this.id] && (delete this.parent.active[this.id], this.domElement.classList.remove('open'));
+        return this.isOpen && (delete this.parent.open[this.id], this.domElement.classList.remove('open')) || true;
     }
     toggle() {
-        this.parent.active[this.id] ? this.close() : this.open();
+        return this.isOpen ? this.close() : this.open();
     }
     remove() {
         delete this.parent.children[this.id];
-        delete this.parent.active[this.id];
+        delete this.parent.open[this.id];
         this.domElement.remove();
     }
     direction(s) {
